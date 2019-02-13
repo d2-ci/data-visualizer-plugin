8fc00cd5f05b40ed613615bc06a0f46149754f79
Ilya Nee <neeilya@icloud.com>
2019-02-13 13:15:20 +0000

    3491344 Merge branch 'master' into feature/open-chart-as-map

--- modules/chartTypes.js
+++
@@ -1,8 +0,0 @@
-export var YEAR_OVER_YEAR_LINE = 'YEAR_OVER_YEAR_LINE';
-export var YEAR_OVER_YEAR_COLUMN = 'YEAR_OVER_YEAR_COLUMN';
-
-var yearOverYearTypes = [YEAR_OVER_YEAR_LINE, YEAR_OVER_YEAR_COLUMN];
-
-export var isYearOverYear = function isYearOverYear(type) {
-  return yearOverYearTypes.includes(type);
-};
