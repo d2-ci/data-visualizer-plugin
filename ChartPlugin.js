773543a13c1095159eb632b0c781023d64956f11
Edoardo Sabadelli <edoardo@dhis2.org>
2019-04-29 13:58:59 +0000

    37c5334 fix(): deep check style for changes

--- ChartPlugin.js
+++ ChartPlugin.js
@@ -64,7 +64,7 @@ var ChartPlugin = function (_Component) {
             }
 
             // id set by DV app, style works in dashboards
-            if (this.props.id !== prevProps.id || this.props.style !== prevProps.style) {
+            if (this.props.id !== prevProps.id || !isEqual(this.props.style, prevProps.style)) {
                 this.recreateChart(0); // disable animation
                 return;
             }
