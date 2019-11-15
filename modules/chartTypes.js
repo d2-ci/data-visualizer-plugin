2c4c1ef892317384709aca8e66ebb46846796810
Martin Ohlson <martin@moid.se>
2019-11-15 09:35:31 +0000

    a12c952 fix: extracted map from vis type to a static item

---
+++ modules/chartTypes.js
@@ -0,0 +1,12 @@
+export var YEAR_OVER_YEAR_LINE = 'YEAR_OVER_YEAR_LINE';
+export var YEAR_OVER_YEAR_COLUMN = 'YEAR_OVER_YEAR_COLUMN';
+export var SINGLE_VALUE = 'SINGLE_VALUE';
+
+var yearOverYearTypes = [YEAR_OVER_YEAR_LINE, YEAR_OVER_YEAR_COLUMN];
+
+export var isYearOverYear = function isYearOverYear(type) {
+  return yearOverYearTypes.includes(type);
+};
+export var isSingleValue = function isSingleValue(type) {
+  return type === SINGLE_VALUE;
+};
