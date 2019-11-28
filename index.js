a92b1c24d15ed22b99952812a2f79f50ef185653
Jan Henrik Overland <janhenrik.overland@gmail.com>
2019-11-28 09:23:21 +0000

    e7f236f fix: merge with epic branch

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-import ChartPlugin from './ChartPlugin';
+import React from 'react';
 
-export default ChartPlugin;
+import ChartPlugin from './ChartPlugin';
+import PivotPlugin from './PivotPlugin';
+
+import { PIVOT_TABLE } from './modules/chartTypes';
+
+var VisualizationPlugin = function VisualizationPlugin(props) {
+    if (!props.config.type || props.config.type === PIVOT_TABLE) {
+        return React.createElement(PivotPlugin, props);
+    } else {
+        return React.createElement(ChartPlugin, props);
+    }
+};
+
+export default VisualizationPlugin;
