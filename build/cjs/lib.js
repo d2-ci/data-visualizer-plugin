c80617eedfb5203cd4b844c4c74ba136277219fa
GitHub <noreply@github.com>
2020-03-11 01:48:38 +0000

    52dce58 Merge branch 'master' into fix/upgrade-d2-try2

--- build/cjs/lib.js
+++ build/cjs/lib.js
@@ -309,7 +309,7 @@ function () {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            req = new d2.analytics.request().fromModel(current).withParameters(options).withIncludeNumDen(true);
+            req = new d2.analytics.request().fromModel(current).withParameters(options).withIncludeNumDen(current.type === analytics.VIS_TYPE_PIVOT_TABLE);
             _context.next = 3;
             return d2.analytics.aggregate.get(req);
 
