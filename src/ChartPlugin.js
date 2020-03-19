66294ae4e419346345d5d5da888042aac3ee5eea
GitHub <noreply@github.com>
2020-03-19 07:56:50 +0000

    9d198d9 chore(deps): bump @material-ui/core from 3.9.3 to 4.9.7

---
+++ src/ChartPlugin.js
@@ -0,0 +1,88 @@
+import React, { useRef, useCallback, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { isSingleValue, createVisualization } from '@dhis2/analytics'
+
+const ChartPlugin = ({
+    visualization,
+    responses,
+    extraOptions,
+    legendSets,
+    id: renderCounter,
+    style,
+    onChartGenerated,
+    animation: defaultAnimation,
+}) => {
+    const canvasRef = useRef(undefined)
+
+    const renderVisualization = useCallback(
+        animation => {
+            const visualizationConfig = createVisualization(
+                responses,
+                visualization,
+                canvasRef.current,
+                {
+                    ...extraOptions,
+                    animation,
+                    legendSets,
+                },
+                undefined,
+                undefined,
+                isSingleValue(visualization.type) ? 'dhis' : 'highcharts' // output format
+            )
+
+            if (isSingleValue(visualization.type)) {
+                onChartGenerated(visualizationConfig.visualization)
+            } else {
+                onChartGenerated(
+                    visualizationConfig.visualization.getSVGForExport({
+                        sourceHeight: 768,
+                        sourceWidth: 1024,
+                    })
+                )
+            }
+        },
+        [
+            canvasRef,
+            visualization,
+            onChartGenerated,
+            responses,
+            extraOptions,
+            legendSets,
+        ]
+    )
+
+    useEffect(() => {
+        renderVisualization(defaultAnimation)
+
+        /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    }, [visualization, responses, extraOptions])
+
+    useEffect(() => {
+        renderVisualization(0)
+
+        /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    }, [renderCounter, style])
+
+    return <div ref={canvasRef} style={style} />
+}
+
+ChartPlugin.defaultProps = {
+    visualization: {},
+    filters: {},
+    style: {},
+    animation: 200,
+    onChartGenerated: Function.prototype,
+}
+
+ChartPlugin.propTypes = {
+    extraOptions: PropTypes.object.isRequired,
+    legendSets: PropTypes.arrayOf(PropTypes.object).isRequired,
+    responses: PropTypes.arrayOf(PropTypes.object).isRequired,
+    visualization: PropTypes.object.isRequired,
+    animation: PropTypes.number,
+    id: PropTypes.number,
+    style: PropTypes.object,
+    onChartGenerated: PropTypes.func,
+}
+
+export default ChartPlugin
